d77303ad3c2f9fc0da28af3714e63718e2c0250c
sjoerdsommen <sjoerdsommen@gmail.com>
2021-09-24 13:54:05 +0200

    finished find best curve pool

--- solidity/scripts/find_best_curve_pool.py
+++ solidity/scripts/find_best_curve_pool.py
@@ -1,4 +1,4 @@
-from brownie import accounts, Contract, ZERO_ADDRESS
+from brownie import accounts, chain, Contract, ZERO_ADDRESS
 from brownie_tokens import MintableForkToken
 
 def load_contract(addr):
@@ -46,7 +46,81 @@ def main():
         key=lambda item: -item[1]
     ):
         print(key, value)
+
+def run_operations(_pool, _pool_index, tripool_lp):
+    # Take a chain snapshot
+    chain.snapshot()
+
+    # Ape into pool
+    ape(_pool, _pool_index, tripool_lp)
+
+    # Skip forward one day
+    chain.mine(timedelta=60*60*24)
+
+    # Check Curve balance
+    _val = calc_curve_value()
+    _name = registry.get_pool_name(_pool)
+
+    # Revert
+    chain.revert()
+    return _name, _val
         
+def ape(pool, pool_index, tripool_lp):
+    # Approve deposit from Tripool into Metapool
+    tripool_bal = tripool_lp.balanceOf(whale)
+    tripool_lp.approve(pool, tripool_bal, {'from': whale})
+
+    # Add Liquidity
+    amounts = [0] * registry.get_n_coins(pool)[0]
+    amounts[pool_index] = tripool_bal
+    pool.add_liquidity(amounts, 0, {'from': whale})
+
+    # Check if the pool has a gauge
+    gauge_addr = registry.get_gauges(pool)[0][0]
+    if gauge_addr == ZERO_ADDRESS:
+        return
+
+    # Create Approval for the rewards pool
+    pool_lp = load_contract(registry.get_lp_token(pool))
+    pool_bal = pool_lp.balanceOf(whale)
+    if pool_lp.allowance(whale, gauge_addr) < pool_bal:
+        pool_lp.approve(gauge_addr, pool_bal, {'from': whale})
+
+    # Make a Deposit
+    gauge = load_contract(gauge_addr)
+    gauge.deposit(pool_bal, {'from': whale})
+
+def calc_curve_value():
+    # Get initial value
+    init_val = calc_balance()
+
+    # Set CRV claim array
+    crv_pools = [ZERO_ADDRESS] * 8
+    j = 0
+    for i in range(registry.pool_count()):
+        # Check if gauge exists
+        _addr = registry.get_gauges(registry.pool_list(i))[0][0]
+        if _addr == ZERO_ADDRESS:
+            continue
+
+        # Add guage to claim if balance
+        _gauge = load_contract(_addr)
+        if _gauge.balanceOf(whale) > 0 and j < 8:
+            crv_pools[j] = _addr
+            j += 1
+
+    # Mint many
+    minter.mint_many(crv_pools, {'from': whale})
+        
+    # Calculate our Balance
+    final_val = calc_balance()
+
+    # Undo Mint Many
+    chain.undo()
+    return final_val - init_val
+
+def calc_balance():
+    return crv.balanceOf(whale) / 10 ** crv.decimals()
 
 def add_tripool_liquidity():
     dai_addr = "0x6B175474E89094C44Da98b954EedeAC495271d0F"
